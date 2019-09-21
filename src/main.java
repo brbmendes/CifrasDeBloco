5ec2d388d7dcfc8029a92dddb254f44e69c18006
brbmendes <bbmendes@gmail.com>
2019-09-21 19:00:47 -0300

    Funciona modo manual. Não com arquivos

--- src/main.java
+++ src/main.java
@@ -19,8 +19,9 @@ public class main {
 			System.out.println("1 - Cifrar texto entrada");
 			System.out.println("2 - Decifrar texto entrada");
 			//System.out.println("3 - Cifrar texto manual");
-			//System.out.println("4 - Deifrar texto manual");
-			System.out.println("5 - Sair");
+			//System.out.println("4 - Decifrar texto manual");
+			System.out.println("5 - Cifra e Decifra texto manual - Op. Mode: CBC");
+			System.out.println("6 - Sair");
 			
 			operacao = s.nextInt();
 			if(operacao == 1) {
@@ -30,6 +31,10 @@ public class main {
 				String cripto = "AES";
 				// Define o arquivo de entrada e converte para um array
 				String caminho = escolherEntrada("cipher");
+				if(caminho.equals("voltar")) {
+					System.out.println("");
+					continue;
+				}
 				String[] entrada = lerEntrada(caminho);
 				
 				// Obtem o vetor de inicializacao
@@ -46,28 +51,29 @@ public class main {
 				
 				// Converte a chave em bytes, e obtem a keySpec
 				SecretKeySpec sKeySpec = AES.getSecretKey(chave);
+				//byte[] byteChave = AES.toByteArray(chave);
+				//SecretKeySpec sKeySpec = new SecretKeySpec(byteChave, 0, byteChave.length, "AES");
 				
-				
+				// Define o modo de operação e padding, se necessário
 				if(tipo.equals("cbc")) {
 					cripto = "AES/CBC/PKCS5Padding";
 				} else {
 					cripto = "AES/CTR/NoPadding";
 				}
+				
 				Cipher cipher = Cipher.getInstance(cripto);
 				
+				// Inicializa o cipher passando a chave e, se necessário, o vetor de inicializacao
 				if(tipo.equals("cbc")) {
 					cipher.init(Cipher.ENCRYPT_MODE, sKeySpec, ivParameterSpec);
 				} else {
 					cipher.init(Cipher.ENCRYPT_MODE, sKeySpec);
 				}
 				
-				byte[] ciphered = cipher.doFinal(texto.getBytes());
+				// Cifra o texto
+				byte[] ciphered = cipher.doFinal(texto.getBytes("UTF-8"));				
 				
-				System.out.println("Tipo: " + tipo.toUpperCase());
-				System.out.println("Chave: " + chave);
-				System.out.println("Texto decifrado: " + texto);
-				System.out.println("Texto cifrado: " + AES.toHexString(ciphered));
-				System.out.println("");
+				imprimirSaidaCifrar(tipo, chave, texto, ciphered);
 			} else if(operacao == 2) {
 				// Decifrar
 				System.out.println("");
@@ -76,6 +82,10 @@ public class main {
 				
 				// Define o arquivo de entrada e converte para um array
 				String caminho = escolherEntrada("decipher");
+				if(caminho.equals("voltar")) {
+					System.out.println("");
+					continue;
+				}
 				String[] entrada = lerEntrada(caminho);
 				
 				// Obtem o vetor de inicializacao
@@ -92,73 +102,107 @@ public class main {
 				
 				// Converte a chave em bytes, e obtem a keySpec
 				SecretKeySpec sKeySpec = AES.getSecretKey(chave);
+				//byte[] byteChave = AES.toByteArray(chave);
+				//SecretKeySpec sKeySpec = new SecretKeySpec(byteChave, 0, byteChave.length, "AES");
 				
+				// Define o modo de operação e o padding, se necessário
 				if(tipo.equals("cbc")) {
 					cripto = "AES/CBC/PKCS5Padding";
+				}  else {
+					cripto = "AES/CTR/NoPadding";
 				}
+				
 				Cipher cipher = Cipher.getInstance(cripto);
 				
+				// Inicializa o cipher passando a chave e, se necessário, o vetor de inicializacao
 				if(tipo.equals("cbc")) {
 					cipher.init(Cipher.ENCRYPT_MODE, sKeySpec, ivParameterSpec);
 				} else {
 					cipher.init(Cipher.ENCRYPT_MODE, sKeySpec);
 				}
 				
+				// Decifra o texto
 				byte[] deciphered = cipher.doFinal(AES.toByteArray(texto));
 				
-				System.out.println("Tipo: " + tipo.toUpperCase());
-				System.out.println("Chave: " + chave);
-				System.out.println("Texto cifrado: " + texto);
-				System.out.println("Texto decifrado: " + (new String(deciphered)));
-				System.out.println("");
+				imprimirSaidaDecifrar(tipo, chave, texto, deciphered);
 			} else if(operacao == 3) {
 				System.out.println("");
 				System.out.println("Digite a senha:");
 				System.out.println("");
 				Scanner scan = new Scanner(System.in);
 				
-				String temp = scan.nextLine();
+				String senha = scan.nextLine();
 				
 				System.out.println("");
 				System.out.println("Digite o texto a ser cifrado:");
-				String temp2 = scan.nextLine();
+				String texto = scan.nextLine();
 				
-				SecretKeySpec sKeySpec = AES.getSecretKey(temp);
+				SecretKeySpec sKeySpec = AES.getSecretKey(senha);
 				
 				Cipher cipher = Cipher.getInstance("AES");
 				
 				cipher.init(Cipher.ENCRYPT_MODE, sKeySpec);
 				
-				byte[] ciphered = cipher.doFinal(temp2.getBytes());
+				byte[] ciphered = cipher.doFinal(texto.getBytes());
 				
-				System.out.println("Senha: " + temp);
-				System.out.println("Texto decifrado: " + temp2);
-				System.out.println("Texto cifrado: " + AES.toHexString(ciphered));
+				imprimirSaidaCifrarManual(senha, texto, ciphered);
 				System.out.println("");
 			} else if(operacao == 4) {
 				System.out.println("Digite a senha:");
 				System.out.println("");
 				Scanner scan = new Scanner(System.in);
 				
-				String temp = scan.nextLine();
+				String senha = scan.nextLine();
 				
 				System.out.println("Digite o texto a ser decifrado:");
-				String temp2 = scan.nextLine();
+				String texto = scan.nextLine();
 				
-				SecretKeySpec sKeySpec = AES.getSecretKey(temp);
+				SecretKeySpec sKeySpec = AES.getSecretKey(senha);
 				
 				Cipher cipher = Cipher.getInstance("AES");
 				
 				cipher.init(Cipher.DECRYPT_MODE, sKeySpec);
 				
-				byte[] deciphered = cipher.doFinal(AES.toByteArray(temp2));
+				byte[] deciphered = cipher.doFinal(AES.toByteArray(texto));
 				
-				System.out.println("");
-				System.out.println("Senha: " + temp);
-				System.out.println("Texto cifrado: " + temp2);
-				System.out.println("Texto decifrado: " + (new String(deciphered)));
+				imprimirSaidaDecifrarManual(senha, texto, deciphered);
 				System.out.println("");
 			} else if(operacao == 5) {
+				//break;
+				System.out.println("");
+				System.out.println("Digite a senha:");
+				System.out.println("");
+				Scanner scan = new Scanner(System.in);
+				
+				String senha = scan.nextLine();
+				SecretKeySpec sKeySpec = AES.getSecretKey(senha);
+				
+				System.out.println("");
+				System.out.println("Digite o texto a ser cifrado:");
+				String texto = scan.nextLine();
+				
+				String vetorInicializacao = "4e657874205468757273646179206f6e";
+				byte[] iv = AES.toByteArray(vetorInicializacao);
+				IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+				
+				
+				Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+				
+				cipher.init(Cipher.ENCRYPT_MODE, sKeySpec, ivParameterSpec);
+				
+				byte[] ciphered = cipher.doFinal(texto.getBytes());
+				
+				imprimirSaidaCifrarManual(senha, texto, ciphered);
+				
+				System.out.println("Digite o texto a ser decifrado:");
+				texto = scan.nextLine();
+				
+				cipher.init(Cipher.DECRYPT_MODE, sKeySpec, ivParameterSpec);
+				
+				byte[] deciphered = cipher.doFinal(AES.toByteArray(texto));
+				
+				imprimirSaidaDecifrarManual(senha, texto, deciphered);
+			} else if(operacao == 6) {
 				break;
 			} else {
 				System.out.println("Operação inválida\n");
@@ -182,12 +226,14 @@ public class main {
 		System.out.println("Escolha um arquivo de entrada: ");
 		
 		if(type.equals("decipher")) {
+			System.out.println("0 - voltar ");
 			System.out.println("1 - tarefa 1 ");
 			System.out.println("2 - tarefa 2 ");
 			System.out.println("3 - tarefa 3 ");
 			System.out.println("4 - tarefa 4 ");
 			//System.out.println("8 - tarefa 8 ");
 		} else {
+			System.out.println("0 - voltar ");
 			System.out.println("5 - tarefa 5 ");
 			System.out.println("6 - tarefa 6 ");
 			//System.out.println("7 - tarefa 7 ");
@@ -218,6 +264,9 @@ public class main {
 			break;
 		case 8:
 			path = "inputs/tarefa_8.txt";
+			break;
+		default:
+			path = "voltar";
 			break;
 		}
 		return path;
@@ -262,5 +311,34 @@ public class main {
 	private static String obterVetorInicializacao(String[] entrada) {
 		String textoCifrado = entrada[1].split(" ")[1];
 		return textoCifrado.substring(0,32);
+	}
+	
+	private static void imprimirSaidaCifrar(String tipo, String chave, String texto, byte[] ciphered) {
+		System.out.println("Tipo: " + tipo.toUpperCase());
+		System.out.println("Chave: " + chave);
+		System.out.println("Texto decifrado: " + texto);
+		System.out.println("Texto cifrado: " + AES.toHexString(ciphered));
+		System.out.println("");
+	}
+	
+	private static void imprimirSaidaDecifrar(String tipo, String chave, String texto, byte[] deciphered) {
+		System.out.println("Tipo: " + tipo.toUpperCase());
+		System.out.println("Chave: " + chave);
+		System.out.println("Texto cifrado: " + texto);
+		System.out.println("Texto decifrado: " + (new String(deciphered)));
+		System.out.println("");
+	}
+	
+	private static void imprimirSaidaCifrarManual(String senha, String texto, byte[] ciphered) {
+		System.out.println("Senha: " + senha);
+		System.out.println("Texto decifrado: " + texto);
+		System.out.println("Texto cifrado: " + AES.toHexString(ciphered));
+		System.out.println("");
+	}
+	
+	private static void imprimirSaidaDecifrarManual(String senha, String texto, byte[] deciphered) {
+		System.out.println("Senha: " + senha);
+		System.out.println("Texto cifrado: " + texto);
+		System.out.println("Texto decifrado: " + (new String(deciphered)));
 	}
 }
